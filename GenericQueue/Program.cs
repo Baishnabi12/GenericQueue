a5d40f8b2526e68ccae56576343b151861cc7a3a
baish <baish@BAISHNABI>
2024-10-17 13:28:20 +0530

    Add project files.

---
+++ GenericQueue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+public class QueueGen
+{
+    static void Main(string[] args)
+    {
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("deleting elements");
+        queue.Dequeue();
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+
+        }
+    }
+}
